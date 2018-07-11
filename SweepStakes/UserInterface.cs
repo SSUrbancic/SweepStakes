24564e35bd0bf3956dae22cd6308fc68fa2da86e
Sam <SSUrbancic@outlook.com>
2018-07-11 16:38:01 -0500

    Created a Sweepstakes class and added a public member called name. In the constructor of Sweepstakes I passed in a name variable and then set that name variable equal to the member variable in the Sweepstakes class using this.name = name. Also, created a method in the UserInterface called DetermineSweepstakesName(). This requires Sweepstakes object to be passed in and then displays a message prompting user input. User input is set equal to sweepstakes.name which is then passed into the Sweepstakes object

--- SweepStakes/UserInterface.cs
+++ SweepStakes/UserInterface.cs
@@ -8,5 +8,49 @@ namespace SweepStakes
 {
     class UserInterface
     {
+        //Member(HAS A)
+
+        //Constructor
+        public UserInterface()
+        {
+
+        }
+        //Methods(Can do)
+        public void DisplayMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+        public void GetContestantInfo(Contestant contestant)
+        {
+            DetermineContestantfirstName(contestant);
+            DetermineContestantLastName(contestant);
+            DetermineContestantEmailAddress(contestant);
+            DetermineContestantRegristrationNumber(contestant);
+        }
+        public void DetermineContestantfirstName(Contestant contestant)
+        {
+            DisplayMessage("Enter Contestant's first name.");
+            contestant.firstName = Console.ReadLine();
+        }
+        public void DetermineContestantLastName(Contestant contestant)
+        {
+            DisplayMessage("Enter Contestant's last name");
+            contestant.lastName = Console.ReadLine();
+        }
+        public void DetermineContestantEmailAddress(Contestant contestant)
+        {
+            DisplayMessage("Enter Contestant's email Address");
+            contestant.emailAddress = Console.ReadLine();
+        }
+        public void DetermineContestantRegristrationNumber(Contestant contestant)
+        {
+            DisplayMessage("Enter Contestant's Registration Number");
+            contestant.registrationNumber = int.Parse(Console.ReadLine());
+        }
+        public void DetermineSweepStakesName(Sweepstakes sweepstakes)
+        {
+            DisplayMessage("Enter name of sweepStakes");
+            sweepstakes.name = Console.ReadLine();
+        }
     }
 }
