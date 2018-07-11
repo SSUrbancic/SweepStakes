07a4fbb32394fa85af92fba74f9a5196206db348
Sam <SSUrbancic@outlook.com>
2018-07-11 15:23:51 -0500

    Created a contestant class and added member variables firstName, lastName, emailAddress, and registrationNumber.

---
+++ SweepStakes/Contestant.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepStakes
+{
+    class Contestant
+    {
+        //Member(HAS A)
+        public string firstName;
+        public string lastName;
+        public string emailAdress;
+        public int registrationNumber;
+        //Constructor
+        public Contestant()
+        {
+
+        }
+        //Methods(Can do)
+    }
+}
