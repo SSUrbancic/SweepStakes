dc734dd40cc241b2ea0c66ec9ad12bdfa43620ce
Sam <SSUrbancic@outlook.com>
2018-07-12 10:22:54 -0500

    Created a SweepstakesStackManager classe that inherited from ISweepstakesManager. Created a new stack called sweepstakesStack. Used InsertSweepstakes method to pass in sweepstakes that are coing to be added to the stack. Using sweepstakesStack.Push the sweepstakes objects are added to the class. In the GetSweepstakes method to pull the sweepstakes from the top of the Stack. Also, created a DisplaySweepstakes method. Using foreach loop it iterates through the Stack and displays the name of each Sweepstakes using Console.Writeline.

---
+++ SweepStakes/SweepstakesStackManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepStakes
+{
+    public class SweepstakesStackManager : ISweepstakesManager
+    {
+        //members
+        Stack<Sweepstakes> sweepstakesStack = new Stack<Sweepstakes>();
+        public void InsertSweepstakes(Sweepstakes sweepstakes)
+        {
+            sweepstakesStack.Push(sweepstakes);
+        }
+        public Sweepstakes GetSweepstakes()
+        {
+            Sweepstakes topOfSweepstakesStack;
+            topOfSweepstakesStack = sweepstakesStack.Pop();
+            return topOfSweepstakesStack;
+        }
+
+        public void DisplaySweepstakesInStack()
+        {
+            Console.WriteLine("Sweepstakes: ");
+            foreach (Sweepstakes sweepstakes in sweepstakesStack)
+            {
+                Console.WriteLine($"This is the {sweepstakes.name} Sweepstakes!");
+            }
+        }
+    }
+}
