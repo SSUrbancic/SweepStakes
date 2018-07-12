bc4ab1efe331a4d0de6bffd8f226a1b60f346c51
Sam <SSUrbancic@outlook.com>
2018-07-12 10:35:30 -0500

    Created a SweepstakesQueueManager class that inherited from ISweepstakesManager. Created a new Queue called sweepstakesQueue. Used InsertSweepstakes method to pass in sweepstakes that are going to be added to the stack. Using sweepstakesStack.Enqueue the sweepstakes objects are added to the class. In the GetSweepstakes method to pull the sweepstakes from the Queue, used sweepstakesQueue.Dequeue() to pull the sweepstakes from the queue. Also, created a DisplaySweepstakes method. Using foreach loop it iterates through the Queue and displays the name of each Sweepstakes using Console.Writeline.

---
+++ SweepStakes/SweepstakesQueueManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepStakes
+{
+    class SweepstakesQueueManager : ISweepstakesManager
+    {
+        //members
+        Queue<Sweepstakes> sweepstakesQueue = new Queue<Sweepstakes>();
+        //Constuctor
+        public SweepstakesQueueManager()
+        {
+
+        }
+        //Methods
+        public void InsertSweepstakes(Sweepstakes sweepstakes)
+        {
+            sweepstakesQueue.Enqueue(sweepstakes);
+        }
+        public Sweepstakes GetSweepstakes()
+        {
+            Sweepstakes topOfSweepstakesQueue;
+            topOfSweepstakesQueue = sweepstakesQueue.Dequeue();
+            return topOfSweepstakesQueue;
+        }
+
+        public void DisplaySweepstakesInQueue()
+        {
+            Console.WriteLine("Sweepstakes: ");
+            foreach (Sweepstakes sweepstakes in sweepstakesQueue)
+            {
+                Console.WriteLine($"This is the {sweepstakes.name} Sweepstakes!");
+            }
+        }
+    }
+}
